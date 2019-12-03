d972b2131e9dcaab8b85dfcc0cd417be280aca39
Rasyid Waterkamp <rasyidwaterkamp@gmail.com>
2019-12-04 01:34:27 +0800

    Initial Commit

---
+++ pract2CTFinal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  pract2CTFinal
+//
+//  Created by Rasyid Waterkamp on 4/12/19.
+//  Copyright © 2019 Rasyid Waterkamp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ pract2CTFinalTests/pract2CTFinalTests.swift
@@ -0,0 +1,34 @@
+//
+//  pract2CTFinalTests.swift
+//  pract2CTFinalTests
+//
+//  Created by Rasyid Waterkamp on 4/12/19.
+//  Copyright © 2019 Rasyid Waterkamp. All rights reserved.
+//
+
+import XCTest
+@testable import pract2CTFinal
+
+class pract2CTFinalTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
